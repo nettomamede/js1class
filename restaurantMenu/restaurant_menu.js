36773cb9f1d0a9cd70e79b382f73858e03fcc1fe
nettomamede <nettomamede@gmail.com>
2025-04-24 08:25:30 -0400

    Added prices on the menu

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict- $22.99', 'Oatmeal- $20.99', 'Frittata- $15'];
+const mainCourseMenu = ['Steak- $25.99', 'Pasta- $22.99', 'Burger- $19.99', 'Salmon- $29.99'];
+const dessertMenu = ['Cake- $12.99', 'Ice Cream- $9.99 ', 'Pudding- $10.99', 'Fruit Salad- $10.99'];
 
 // Breakfast
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
