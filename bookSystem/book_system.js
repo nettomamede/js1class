13dd408223a7972a43521b3ac822e965d7644690
nettomamede <nettomamede@gmail.com>
2025-04-29 09:11:57 -0400

    Added delete buttom

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -21,15 +21,18 @@ function addBook() {
 }
 
 function showbooks() {
-    const booksDiv = books.map((book, index) => `<h1>book Number: ${index + 1}</h1>
-        <p><strong>Book Name: </strong>${book.name}</p>
+    const booksDiv = books.map((book, index) => `
+        <h1>Book Number: ${index + 1}</h1>
+        <p><strong>Book Name:</strong> ${book.name}</p>
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
         <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
-        <button onclick="editbook(${index})">Edit</button>`
-    );
+        <button onclick="editbook(${index})">Edit</button>
+        <button onclick="deleteBook(${index})">Delete</button>
+    `);
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
+
 
 function editbook(index) {
     const book = books[index];
@@ -46,4 +49,10 @@ function clearInputs() {
     document.getElementById('authorName').value = '';
     document.getElementById('bookDescription').value = '';
     document.getElementById('pagesNumber').value = '';
+}
+
+function deleteBook(index) {
+    // Remove the book entry at the given index
+    books.splice(index, 1);
+    showbooks(); // Refresh the book list after deletion
 }
